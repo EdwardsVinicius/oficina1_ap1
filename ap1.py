a19b915de6957e46b28cf3feaabdcecb58dd613e
vinicius <vinicius.edwards@gmail.com>
2023-03-02 14:44:14 -0400

    foi pora aaaaaa

--- ap1.py
+++ ap1.py
@@ -1,8 +1,10 @@
 from tkinter import *
-import recommender as rec
+from tkinter import messagebox
+import json
 
 class App:
     def __init__(self, master=None):
+
         self.container1 = Frame(master)
         self.container1['pady'] = 10
         self.container1.pack()
@@ -32,15 +34,11 @@ class App:
         self.container7['pady'] = 20
         self.container7.pack()
 
-        self.title = Label(self.container1, text='Sistema de Recomendação de Jogos')
-        self.title.pack()
-
         # User
         self.userLabel = Label(self.container3, text='Nome do usuário')
         self.userLabel.pack(side=LEFT)
 
         self.user = Entry(self.container3, textvariable = StringVar())
-        self.user['width'] = 30
         self.user.pack(side=LEFT)
 
         # Game name
@@ -48,7 +46,6 @@ class App:
         self.gameLabel.pack(side=LEFT)
 
         self.game = Entry(self.container4, textvariable = StringVar())
-        self.game['width'] = 30
         self.game.pack(side=LEFT)
 
         # Avaliação
@@ -56,7 +53,6 @@ class App:
         self.ratingLabel.pack(side=LEFT)
 
         self.rating = Entry(self.container5, textvariable = IntVar())
-        self.rating['width'] = 30
         self.rating.pack(side=LEFT)
 
         # Número de recomendações
@@ -64,46 +60,61 @@ class App:
         self.qtdRecLabel.pack(side=LEFT)
 
         self.qtdRec = Entry(self.container6, textvariable = IntVar())
-        self.qtdRec['width'] = 30
         self.qtdRec.pack(side=LEFT)
 
         # Botoes
-        self.recommend = Button(self.container7, text='Recomendar Jogos')
-        self.recommend['width'] = 12
-        self.recommend['command'] = self.recommend
-        self.recommend.pack(side=RIGHT)
+        self.recomm = Button(self.container7, text='Recomendar Jogos')
+        self.recomm['command'] = self.recommend
+        self.recomm.pack(side=RIGHT)
 
-        self.recommend = Button(self.container7, text='Enviar Avaliação')
-        self.recommend['width'] = 12
-        self.recommend['command'] = self.submit_rating
-        self.recommend.pack(side=RIGHT)
+        self.submit = Button(self.container7, text='Enviar Avaliação')
+        self.submit['command'] = self.submit_rating
+        self.submit.pack(side=RIGHT)
 
         self.quit = Button(self.container7, text='Sair')
-        self.quit['width'] = 12
         self.quit['command'] = root.destroy
         self.quit.pack(side=RIGHT)
+
+    def recommend_game(self, user, k):
+        games = {}
+        for d in data:
+            if d["user"] != user:
+                if d["game_title"] not in games:
+                    games[d["game_title"]] = {"rating": 0, "votes": 0}
+                games[d["game_title"]]["rating"]  = d["rating"]
+                games[d["game_title"]]["votes"]  = 1
+        for game in games:
+            games[game]["rating"] = games[game]["rating"] / games[game]["votes"]
+        rec_games = []
+        for game in games:
+            if games[game]["rating"] >= 4:
+                rec_games.append(game)
+
+        return rec_games[:int(k)]
 
     def recommend(self):
         user = self.user.get()
         qtdRec = self.qtdRec.get()
-        rec.recommend(user, qtdRec)
+        rec_games = self.recommend_game(user, qtdRec)
+        messagebox.showinfo("Recomendações", f"Os {qtdRec} jogos recomendados para o usuário {user} são: {rec_games}")
 
     def submit_rating(self):
         game = self.game.get()
         rating = self.rating.get()
         user = self.user.get()
-        rec.submit_rating(game, rating, user)
+        if (game == '' or rating == 0 or user == ''):
+            messagebox.showinfo("Erro", "Preencha os campos corretamente")
+        else:
+            data.append({"user": user, "game_title": game, "rating": rating})
+            messagebox.showinfo("Sucesso", f"Avaliação do jogo {game} foi adicionada com sucesso")
 
 
+f = open('game_ratings.json')
+data = json.load(f)
 
 root = Tk()
+root.title('Sistema de Recomendação de Jogos')
 
-# frm = ttk.Frame(root, padding=10)
-# frm.grid()
-# ttk.Label(frm, text='Sistema de Recomendação').pack()
-
-# ttk.Entry(root).pack()
-
-# ttk.Button(frm, text='Quit', command=root.destroy).pack()
 App(root)
 root.mainloop()
+f.close()
--- recommender.py
+++ recommender.py
@@ -1,4 +1,4 @@
-# import tkinter as tk
+import tkinter as tk
 from tkinter import messagebox
 import json
 
@@ -21,58 +21,57 @@ def recommend_song(user, k):
         if games[game]["rating"] >= 4:
             rec_games.append(game)
 
-    print('chegou aqui')
-    return rec_games[:int(k)]
+    return rec_games[:k]
 
 def submit_rating(game, rating, user):
-    # game = game_var.get()
-    # rating = rating_var.get()
-    # user = user_var.get()
+    game = game_var.get()
+    rating = rating_var.get()
+    user = user_var.get()
     data.append({"user": user, "game_title": game, "rating": rating})
     messagebox.showinfo("Sucesso", f"Avaliação do jogo {game} foi adicionada com sucesso")
     
 def recommend(user, k):
-    # user = user_var.get()
-    # k = k_var.get()
+    user = user_var.get()
+    k = k_var.get()
     rec_games = recommend_song(user, k)
     messagebox.showinfo("Recomendações", f"Os {k} jogos recomendados para o usuário {user} são: {rec_games}")
 
 
-# root = tk.Tk()
-# root.title("Sistema de Recomendação de Música")
+root = tk.Tk()
+root.title("Sistema de Recomendação de Música")
 
-# user_label = tk.Label(root, text="Nome do usuário:")
-# user_label.grid(row=0, column=0)
+user_label = tk.Label(root, text="Nome do usuário:")
+user_label.grid(row=0, column=0)
 
-# user_var = tk.StringVar()
-# user_entry = tk.Entry(root, textvariable=user_var)
-# user_entry.grid(row=0, column=1)
+user_var = tk.StringVar()
+user_entry = tk.Entry(root, textvariable=user_var)
+user_entry.grid(row=0, column=1)
 
-# song_label = tk.Label(root, text="Nome da música:")
-# song_label.grid(row=1, column=0)
+song_label = tk.Label(root, text="Nome da música:")
+song_label.grid(row=1, column=0)
 
-# game_var = tk.StringVar()
-# song_entry = tk.Entry(root, textvariable=game_var)
-# song_entry.grid(row=1, column=1)
+game_var = tk.StringVar()
+song_entry = tk.Entry(root, textvariable=game_var)
+song_entry.grid(row=1, column=1)
 
-# rating_label = tk.Label(root, text="Avaliação (1-5):")
-# rating_label.grid(row=2, column=0)
+rating_label = tk.Label(root, text="Avaliação (1-5):")
+rating_label.grid(row=2, column=0)
 
-# rating_var = tk.IntVar()
-# rating_entry = tk.Entry(root, textvariable=rating_var)
-# rating_entry.grid(row=2, column=1)
+rating_var = tk.IntVar()
+rating_entry = tk.Entry(root, textvariable=rating_var)
+rating_entry.grid(row=2, column=1)
 
-# k_label = tk.Label(root, text="Número de recomendações:")
-# k_label.grid(row=3, column=0)
+k_label = tk.Label(root, text="Número de recomendações:")
+k_label.grid(row=3, column=0)
 
-# k_var = tk.IntVar()
-# k_entry = tk.Entry(root, textvariable=k_var)
-# k_entry.grid(row=3, column=1)
+k_var = tk.IntVar()
+k_entry = tk.Entry(root, textvariable=k_var)
+k_entry.grid(row=3, column=1)
 
-# submit_button = tk.Button(root, text="Enviar avaliação", command=submit_rating)
-# submit_button.grid(row=4, column=0)
+submit_button = tk.Button(root, text="Enviar avaliação", command=submit_rating)
+submit_button.grid(row=4, column=0)
 
-# recommend_button = tk.Button(root, text="Recomendar músicas", command=recommend)
-# recommend_button.grid(row=4, column=1)
+recommend_button = tk.Button(root, text="Recomendar músicas", command=recommend)
+recommend_button.grid(row=4, column=1)
 
-# root.mainloop()
+root.mainloop()
