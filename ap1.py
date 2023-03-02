87efa37153014c5b79decd8acec20ade31ac5f22
vinicius <vinicius.edwards@gmail.com>
2023-03-02 13:52:49 -0400

    aaaaaaaaaaaaaaa

--- ap1.py
+++ ap1.py
@@ -1,11 +1,109 @@
 from tkinter import *
-from tkinter import ttk
-import csv
+import recommender as rec
+
+class App:
+    def __init__(self, master=None):
+        self.container1 = Frame(master)
+        self.container1['pady'] = 10
+        self.container1.pack()
+
+        self.container2 = Frame(master)
+        self.container2['padx'] = 20
+        self.container2.pack()
+
+        self.container3 = Frame(master)
+        self.container3['padx'] = 20
+        self.container3.pack()
+
+        self.container4 = Frame(master)
+        self.container4['padx'] = 20
+        self.container4.pack()
+
+        self.container5 = Frame(master)
+        self.container5['padx'] = 20
+        self.container5.pack()
+
+        self.container6 = Frame(master)
+        self.container6['padx'] = 20
+        self.container6.pack()
+
+        self.container7 = Frame(master)
+        self.container7['padx'] = 20
+        self.container7['pady'] = 20
+        self.container7.pack()
+
+        self.title = Label(self.container1, text='Sistema de Recomendação de Jogos')
+        self.title.pack()
+
+        # User
+        self.userLabel = Label(self.container3, text='Nome do usuário')
+        self.userLabel.pack(side=LEFT)
+
+        self.user = Entry(self.container3, textvariable = StringVar())
+        self.user['width'] = 30
+        self.user.pack(side=LEFT)
+
+        # Game name
+        self.gameLabel = Label(self.container4, text='Nome do jogo')
+        self.gameLabel.pack(side=LEFT)
+
+        self.game = Entry(self.container4, textvariable = StringVar())
+        self.game['width'] = 30
+        self.game.pack(side=LEFT)
+
+        # Avaliação
+        self.ratingLabel = Label(self.container5, text='Avaliação (1-5)')
+        self.ratingLabel.pack(side=LEFT)
+
+        self.rating = Entry(self.container5, textvariable = IntVar())
+        self.rating['width'] = 30
+        self.rating.pack(side=LEFT)
+
+        # Número de recomendações
+        self.qtdRecLabel = Label(self.container6, text='N. de Recomendações')
+        self.qtdRecLabel.pack(side=LEFT)
+
+        self.qtdRec = Entry(self.container6, textvariable = IntVar())
+        self.qtdRec['width'] = 30
+        self.qtdRec.pack(side=LEFT)
+
+        # Botoes
+        self.recommend = Button(self.container7, text='Recomendar Jogos')
+        self.recommend['width'] = 12
+        self.recommend['command'] = self.recommend
+        self.recommend.pack(side=RIGHT)
+
+        self.recommend = Button(self.container7, text='Enviar Avaliação')
+        self.recommend['width'] = 12
+        self.recommend['command'] = self.submit_rating
+        self.recommend.pack(side=RIGHT)
+
+        self.quit = Button(self.container7, text='Sair')
+        self.quit['width'] = 12
+        self.quit['command'] = root.destroy
+        self.quit.pack(side=RIGHT)
+
+    def recommend(self):
+        user = self.user.get()
+        qtdRec = self.qtdRec.get()
+        rec.recommend(user, qtdRec)
+
+    def submit_rating(self):
+        game = self.game.get()
+        rating = self.rating.get()
+        user = self.user.get()
+        rec.submit_rating(game, rating, user)
+
+
 
 root = Tk()
 
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text='Hello World').grid(column=0, row=0)
-ttk.Button(frm, text='Quit', command=root.destroy).grid(column=1, row=0)
+# frm = ttk.Frame(root, padding=10)
+# frm.grid()
+# ttk.Label(frm, text='Sistema de Recomendação').pack()
+
+# ttk.Entry(root).pack()
+
+# ttk.Button(frm, text='Quit', command=root.destroy).pack()
+App(root)
 root.mainloop()
--- datasetReader.py
+++ datasetReader.py
@@ -1,6 +1,115 @@
-import csv
-import json
+from math import sqrt
 
+users = {
+    "Vinicius": {
+        "Dark Souls":2.5,
+        "God of War":0.7,
+        "The Legend of Zelda: Breath of the Wild":3.2,
+        "Minecraft":1.7,
+        "The Last of Us":4.7,
+        "Terraria":4.8,
+        "Hollow Knight":0.0,
+        "Persona 5 Royal":1.2,
+        "Super Mario Odyssey":2.4,
+        "Call of Duty: Black Ops II":3.3,
+        "Super Mario 64":4.0,      
+    },
+    "Evandro": {
+        "Portal 2":4.7,
+        "Super Mario Galaxy":3.6,
+        "The Elder Scrolls V: Skyrim":1.1,
+        "Red Dead Redemption 2":1.4,
+        "Minecraft":2.7,
+        "Left 4 Dead 2":2.9,
+        "Uncharted 4: A Thief's End":3.8,
+        "Call of Duty: Modern Warfare 2":3.5,
+        "The Last of Us Part II":4.6,
+        "Super Smash Bros. Ultimate":4.5,
+        "Grand Theft Auto V":3.4,
+        "The Legend of Zelda: Breath of the Wild":3.3,
+        "Left 4 Dead 2":0.9,
+        "Team Fortress 2":2.7,
+        "Persona 5 Royal":4.6,
+    },
+    "Marvin": {
+        "Grand Theft Auto: San Andreas":4.7,
+        "Grand Theft Auto V":4.5,
+        "The Legend of Zelda: Ocarina of Time":2.0,
+        "Red Dead Redemption 2":0.8,
+        "The Legend of Zelda: Breath of the Wild":0.3,
+        "Super Mario Galaxy":4.6,
+        "Halo 3":2.3,
+        "Stardew Valley":2.2,
+    },
+    "Caio": {
+        "Super Mario Odyssey":0.2,
+        "Hollow Knight":2.0,
+        "Persona 5 Royal":1.1,
+        "Team Fortress 2":4.5,
+        "Grand Theft Auto: San Andreas":0.1,
+        "Super Smash Bros. Ultimate":2.5,
+        "The Last of Us Part II":4.9,
+        "Grand Theft Auto V":3.6,
+        "Portal 2":1.3,
+        "Halo 3":4.1,
+        "Cuphead":3.8,
+        "Red Dead Redemption":0.5,
+        "The Witcher 3: Wild Hunt":1.6,
+    },
+    "Hugo": {
+        "Undertale":0.9,
+        "Persona 5 Royal":0.0,
+        "Halo 3":4.8,
+        "Portal 2":1.8,
+        "Super Mario Galaxy":1.6,
+        "Call of Duty: Black Ops II":3.9,
+        "The Last of Us":2.9,
+        "Grand Theft Auto: San Andreas":1.0,
+        "The Elder Scrolls V: Skyrim":4.6,
+        "Call of Duty: Black Ops II":4.4,
+        "God of War":0.4,
+        "Red Dead Redemption":1.8,
+    },
+    "Pique": {
+        "Super Mario Galaxy":3.3,
+        "Halo 3":1.7,
+        "The Elder Scrolls V: Skyrim":1.1,
+        "Super Mario 64":0.5,
+        "Red Dead Redemption":3.1,
+        "The Witcher 3: Wild Hunt":3.8,
+        "Undertale":3.7,
+        "Uncharted 4: A Thief's End":1.6,
+        "Cuphead":1.0,
+        "Team Fortress 2":0.1,
+        "The Last of Us Part II":0.8,
+    },
+    "Shakira": {
+        "Left 4 Dead 2":3.1,
+        "Red Dead Redemption 2":0.6,
+        "The Witcher 3: Wild Hunt":3.2,
+        "Super Mario Galaxy":1.9,
+        "Halo 3":4.6,
+        "God of War":2.6,
+        "Grand Theft Auto V":2.5,
+        "Cuphead":4.6,
+        "Call of Duty: Modern Warfare 2":2.2,
+        "The Legend of Zelda: Ocarina of Time":0.3,
+        "Persona 5 Royal":4.5,
+    },
+    "John": {
+        "Halo 3":4.3,
+        "Stardew Valley":3.5,
+        "The Last of Us":4.5,
+        "Red Dead Redemption 2":0.6,
+        "Left 4 Dead 2":4.7,
+        "The Witcher 3: Wild Hunt":2.5,
+        "Undertale":0.1,
+        "Minecraft":4.1,
+        "Dark Souls":1.8,
+        "The Legend of Zelda: Breath of the Wild":4.0,
+        "The Legend of Zelda: Ocarina of Time":4.0,
+    }
+}
 
 def pearson(rating1, rating2):
     sum_xy = 0
@@ -47,7 +156,7 @@ def manhattan(rating1, rating2):
     commonRatings = False
     for key in rating1:
         if key in rating2:
-            distance  = abs(rating1['ratings'] - rating2['ratings'])
+            distance  = abs(rating1[key] - rating2[key])
             commonRatings = True
     if commonRatings:
         return distance
@@ -55,52 +164,38 @@ def manhattan(rating1, rating2):
         return -1  #Indicates no ratings in common
 
 
-def computeNearestNeighbor(username, reader):
-    """creates a sorted list of users based on their distance to username"""
+def computeNearestNeighbor(username, users):
     distances = []
-    for row in reader:
-        if row['name'] != username:
-            distance = manhattan(row, user_row)
-            distances.append((distance, row))
+    for user in users:
+        if user != username:
+            distance = manhattan(users[user], users[username])
+            distances.append((distance, user))
     # sort based on distance -- closest first
     distances.sort()
     return distances
 
 
-def recommend(username, reader):
-    """Give list of recommendations"""
+def recommend(username, users=users):
     # first find nearest neighbor
-    username
-    nearest = computeNearestNeighbor(username, reader)[0][1]
+    nearest = computeNearestNeighbor(username, users)[0][1]
 
     recommendations = []
     # now find bands neighbor rated that user didn't
-    neighborRatings = reader[nearest]
-    userRatings = reader[username]
-    for movie in neighborRatings:
-        if not movie in userRatings:
-            recommendations.append((movie, neighborRatings[movie]))
+    neighborRatings = users[nearest]
+    userRatings = users[username]
+    for artist in neighborRatings:
+        if not artist in userRatings:
+            recommendations.append((artist, neighborRatings[artist]))
     # using the fn sorted for variety - sort is more efficient
     return sorted(recommendations,
-                  key=lambda movieTuple: movieTuple[1],
+                  key=lambda artistTuple: artistTuple[1],
                   reverse=True)
 
 
-# with open('data_set.csv', newline='') as csvfile:
-#     reader = csv.DictReader(csvfile)
-#     for row in reader:
-#         print(row)
-    # print(recommend('Vivia Denisyev', reader))
+# examples - uncomment to run
 
-username = ''
-user_row = ''
+# print(users["Veronica"])
+# print(manhattan(users["Hailey"], users["Veronica"]))
 
-file = open('ratings.json')
-data = json.load(file)
-for i in data:
-    if (i['name'] == username):
-        user_row = i['name']
-    print(i)
-
-print(recommend(username, data))
-file.close()
+# print(computeNearestNeighbor("Dan", users))
+# print(recommend("Vinicius", users))
